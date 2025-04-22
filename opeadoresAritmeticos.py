078a4c549b1a525b2cad0e8c29a2d34faae6535b
CARLOS ANAEL PEREIRA PINTO <202404493199@alunos.unifanor.edu.br>
2025-04-22 20:16:36 -0300

    novo assunto

---
+++ opeadoresAritmeticos.py
@@ -0,0 +1,8 @@
+# Crie duas variaveis que recebe, respectivamentes dois numeros.
+numero1 = 10
+numero2 = 15# Imprima na tela:
+# Soma
+# Subtração
+# Mulplicação
+# Divisão
+# Potencia
---
+++ variaveis.py
@@ -0,0 +1,8 @@
+
+nome = "Anael"
+curso = "Eng. Civil"
+anoCurso = "3° semestre"
+ira = 9.4
+print(nome,curso, anoCurso, ira)
+
+print(f"Teu nome é {nome}, você cursa {curso}, está no {anoCurso} e o teu IRA é de {ira} pontos. Parabéns")
